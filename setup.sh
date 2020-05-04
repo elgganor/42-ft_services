839b9541c4577a9ec28b7da74f7cdde417775e24
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2020-05-04 13:55:35 +0200

    Mise en place de influxdb et des volumes persistent

--- setup.sh
+++ setup.sh
@@ -14,20 +14,30 @@ sed -i '' "s/MINIKUBE_IP/$IP/g" srcs/mysql/wordpress.sql
 cp srcs/ftps/setup_template.sh srcs/ftps/setup.sh
 sed -i '' "s/MINIKUBE_IP/$IP/g" srcs/ftps/setup.sh
 
+
+# ===== Docekr images =====
 docker build -t nginx srcs/nginx
 docker build -t mysql srcs/mysql
 docker build -t phpmyadmin srcs/phpmyadmin
 docker build -t wordpress srcs/wordpress
 docker build -t ftps srcs/ftps
+docker build -t influxdb srcs/influxdb
 
+
+# ===== Deployment =====
 kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/nginx/ingress.yaml
 kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/mysql/mysql-pv.yaml
+kubectl apply -f srcs/mysql/mysql-pv-claim.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
 kubectl apply -f srcs/ftps/ftps.yaml
+kubectl apply -f srcs/influxdb/influxdb.yaml
+kubectl apply -f srcs/influxdb/influxdb-pv.yaml
+kubectl apply -f srcs/influxdb/influxdb-pv-claim.yaml
 
-# state=$(kubectl describe pod $(kubectl get pods | grep mysql | cut -d" " -f1) | grep Status: | cut -d" " -f10)
+
 state="Pending"
 until [ $state == "Running" ]; do
 	state=$(kubectl describe pod $(kubectl get pods | grep mysql | cut -d" " -f1) | grep Status: | cut -d" " -f10)
